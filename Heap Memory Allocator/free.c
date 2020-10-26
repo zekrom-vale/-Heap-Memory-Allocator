7340359201099321910ecfa942f6a3e0b5e61134
Shawn <sgraven66@gmail.com>
2020-10-26 15:45:03 -0500

    Append to void* fix

--- Heap Memory Allocator/free.c
+++ Heap Memory Allocator/free.c
@@ -1,13 +1,13 @@
 #include "free.h"
 
 struct header* getHead(void* start){
-	return (struct header*)(start - HEADER_SIZE);
+	return (struct header*)ptrSub(start, HEADER_SIZE);
 }
 
 int Mem_Free(void* ptr){
 	struct header* head=getHead(ptr);
 	assert(head->magic==MAGIC);
 	size_t size=head->size;
-	coalesce(add((void*)head, size));
+	coalesce(add(head, size));
 	return 0;
 }
