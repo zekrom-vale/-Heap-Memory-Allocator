45e03c3194be319612e454d86698358fb9668397
Shawn <sgraven66@gmail.com>
2020-11-04 09:47:12 -0600

    Moved functions to alloc as it was appropreate

--- alloc.h
+++ alloc.h
@@ -1,42 +1,61 @@
 #ifndef ALLOC_H
 #define ALLOC_H
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#include "error.h"
 #include "linkedList.h"
 #include "listFind.h"
-#include "util.h"
 #include "mem.h"
-#include "error.h"
-#include <stdlib.h>
-#include <stddef.h>
-#include <stdbool.h>
+#include "util.h"
 
-/** 
+/**
+ * Fix MAP_ANONYMOUS not defined
+ * Likely an issue with Visual Studio and
+ * WSL
+ */
+#ifndef MAP_ANONYMOUS
+#define MAP_ANONYMOUS 0x20
+#define MAP_ANON MAP_ANONYMOUS
+#endif
+/**
+ * If true pass the page size to mmap, 0 otherwise
+ */
+#define PAGE false
+
+void* alloc_request(size_t size);
+
+/**
  * The magic value to validate headers
  */
 #define MAGIC 0x144b36e8
-/** 
+/**
  * The chunk of space to allocate at a time
  * mmaped space will be rounded to this value
  * Should be a factor of ALIGN
  */
 #define CHUNK 0x4000UL
-/** 
+/**
  * When true expand the mmaped space
  */
 #define EXPAND true
-/** 
+/**
  * The amount to align allocated space by
  * Rounds up allocated space to this ammount
  * Use 0x1 to disable
  */
 #define ALIGN 0x8
 
-/** 
+/**
  * The header of the allocated space
  * @member magic the validation constant of the header
  * @member size the size of the allocated space
  */
-struct header{
+struct header {
 	int magic;
 	size_t size;
 };
