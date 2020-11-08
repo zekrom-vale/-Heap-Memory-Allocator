bad6b73bc66d82fbc8113a9005bb3f8fd1cde79e
Shawn <sgraven66@gmail.com>
2020-11-08 11:33:50 -0600

    Revert Revert Moved functions into alloc

--- init.c
+++ init.c
@@ -1,32 +1,6 @@
 #include "init.h"
 
 /**
- * requests a new chunk of memory
- * @param size the size of the block
- * @return the pointer of the start of the mmap
- */
-void* init_request(size_t size){
-	void* v=mmap(
-		NULL,
-		size,
-		PROT_READ|PROT_WRITE,
-		MAP_PRIVATE|MAP_ANONYMOUS,
-		//Don't need file device (fd) with MAP_ANONYMOUS
-		0,
-		#if PAGE
-		sysconf(_SC_PAGE_SIZE)
-		#else
-		0
-		#endif
-	);
-	if(v == MAP_FAILED){
-		//perror("mmap");
-		return NULL;
-	}
-    return v;
-}
-
-/**
  * Initalises the first chunk of mmaped memory
  * If this space runs out it will be automaticaly expaded relative to the size of the requested memory, but enough for multiple allocations
  * @param sizeOfRegion The size of the memory, this will be rounded to the nerest CHUNK
--- init.h
+++ init.h
@@ -2,26 +2,11 @@
 #define INIT_H
 
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include "mem.h"
+
+#include "alloc.h"
 #include "linkedList.h"
 #include "alloc.h"
 
-/**
- * Fix MAP_ANONYMOUS not defined
- * Likely an issue with Visual Studio and WSL
- */
-#ifndef MAP_ANONYMOUS
-#define MAP_ANONYMOUS   0x20
-#define MAP_ANON        MAP_ANONYMOUS
-#endif
-/** 
- * If true pass the page size to mmap, 0 otherwise
- */
-#define PAGE false
-
-void* init_request(size_t size);
 int Mem_Free(void* ptr);
 struct linkedList* init_list(struct linkedList* ptr);
 
