c82f519f8fd87b4da9ef43e572c70f26cc687572
Shawn <sgraven66@gmail.com>
2020-11-09 19:57:08 -0600

    Allows scalling the sizes

--- main.c
+++ main.c
@@ -13,19 +13,24 @@ int main(int argc, char* argv[]){
 	Mem_Init(ALLOC_SIZE);
 	printf("Init\n");
 	Mem_Dump();
-	if(argc<=1)main_default();
-	else if(strcmp("default", argv[1]))main_default();
-	else if(strcmp("small", argv[1]))main_small();
+	if(argc<=1){
+		main_default(1);
+		return;
+	}
+	int mult;
+	if(argc>=2)mult=argv[2];
+	else mult=1;
+	if(strcmp("default", argv[1]))main_default(mult);
+	else if(strcmp("small", argv[1]))main_small(mult);
 	else if(strcmp("break", argv[1]))main_break();
 	else if(strcmp("breakNeg", argv[1]))main_breakNeg();
 	else if(strcmp("breakZero", argv[1]))main_breakZero();
 	else if(strcmp("breakBad", argv[1]))main_breakBad();
 	else if(strcmp("breakNULL", argv[1]))main_breakNULL();
-	else main_small();
+	else main_small(mult);
 }
 
-void main_default(){
-
+void main_default(int mult){
 	void* a = Mem_Alloc(ALLOC_SIZE);
 
 #if MM_FREE
@@ -34,19 +39,19 @@ void main_default(){
 	Mem_Dump();
 #endif
 
-	void* a1=Mem_Alloc(100);
+	void* a1=Mem_Alloc(100*mult);
 	printf("Alloc 100 a1\n");
 	Mem_Dump();
 
-	void* a2 = Mem_Alloc(200);
+	void* a2 = Mem_Alloc(200*mult);
 	printf("Alloc 200 a2\n");
 	Mem_Dump();
 
-	void* a3 = Mem_Alloc(10);
+	void* a3 = Mem_Alloc(10*mult);
 	printf("Alloc 10 a3\n");
 	Mem_Dump();
 
-	void* a4 = Mem_Alloc(2000);
+	void* a4 = Mem_Alloc(2000*mult);
 	printf("Alloc 2000 a4\n");
 	Mem_Dump();
 
@@ -67,49 +72,49 @@ void main_default(){
 	printf("Free a4\n");
 	Mem_Dump();
 #endif
-	a1=Mem_Alloc(310);
+	a1=Mem_Alloc(310*mult);
 	printf("Alloc 310 a1\n");
 	Mem_Dump();
 	printf("Alloc 1 a2\n");
-	a2 = Mem_Alloc(1);
+	a2 = Mem_Alloc(1*mult);
 	Mem_Dump();
 
 	//Force it to expand memory
-	void* b = Mem_Alloc(2 * ALLOC_SIZE);
+	void* b = Mem_Alloc(2 * ALLOC_SIZE*mult);
 	printf("Alloc 2ALLOC_SIZE a\n");
 	Mem_Dump();
 }
 
-void main_small(){
-	void* a1=Mem_Alloc(12);
+void main_small(int mult){
+	void* a1=Mem_Alloc(12*mult);
 	printf("Alloc 12 a1\n");
 	Mem_Dump();
 
-	void* a2 = Mem_Alloc(22);
+	void* a2 = Mem_Alloc(22*mult);
 	printf("Alloc 22 a2\n");
 	Mem_Dump();
 
-	void* a3 = Mem_Alloc(20);
+	void* a3 = Mem_Alloc(20*mult);
 	printf("Alloc 20 a3\n");
 	Mem_Dump();
 
-	void* a4 = Mem_Alloc(123);
+	void* a4 = Mem_Alloc(123*mult);
 	printf("Alloc 123 a4\n");
 	Mem_Dump();
 
-	void* a5=Mem_Alloc(100);
+	void* a5=Mem_Alloc(100*mult);
 	printf("Alloc 100 a1\n");
 	Mem_Dump();
 
-	void* a6 = Mem_Alloc(200);
+	void* a6 = Mem_Alloc(200*mult);
 	printf("Alloc 200 a2\n");
 	Mem_Dump();
 
-	void* a7 = Mem_Alloc(120);
+	void* a7 = Mem_Alloc(120*mult);
 	printf("Alloc 120 a3\n");
 	Mem_Dump();
 
-	void* a8 = Mem_Alloc(20);
+	void* a8 = Mem_Alloc(20*mult);
 	printf("Alloc 20 a4\n");
 	Mem_Dump();
 
@@ -131,11 +136,11 @@ void main_small(){
 	Mem_Dump();
 #endif
 
-	a2 = Mem_Alloc(232);
+	a2 = Mem_Alloc(232*mult);
 	printf("Alloc 232 a2\n");
 	Mem_Dump();
 
-	a3 = Mem_Alloc(30);
+	a3 = Mem_Alloc(30*mult);
 	printf("Alloc 30 a3\n");
 	Mem_Dump();
 
